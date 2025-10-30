82a9dab431a6f9ca03c37ec4dcce47138d7a4ad4
kaitchin12 <cauaamorim05@gmail.com>
2025-10-30 01:51:25 -0300

    alterações na base de saldo e nova transacao

--- dist/js/saldoComponent.js
+++ dist/js/saldoComponent.js
@@ -1,5 +1,15 @@
 let saldo = 3000;
 const elementoSaldo = document.querySelector(".saldo-valor .valor");
+const elementoSaldoDataAcesso = document.querySelector(".block-saldo time");
 if (elementoSaldo != null) {
-    elementoSaldo.textContent = saldo.toString();
+    elementoSaldo.textContent = saldo.toLocaleString("pt-br", { currency: "BRL", style: "currency" });
+}
+if (elementoSaldoDataAcesso != null) {
+    const dataAtualHoje = new Date();
+    elementoSaldoDataAcesso.textContent = dataAtualHoje.toLocaleDateString("pt-br", {
+        weekday: "long",
+        month: "2-digit",
+        day: "2-digit",
+        year: "numeric"
+    });
 }
--- src/saldoComponent.ts
+++ src/saldoComponent.ts
@@ -1,7 +1,20 @@
-let saldo : number = 3000;
+let saldo: number = 3000;
 
 
 const elementoSaldo = document.querySelector(".saldo-valor .valor") as HTMLElement;
+const elementoSaldoDataAcesso = document.querySelector(".block-saldo time") as HTMLElement;
+
+
 if (elementoSaldo != null) {
     elementoSaldo.textContent = saldo.toLocaleString("pt-br", { currency: "BRL", style: "currency" });
+}
+
+if (elementoSaldoDataAcesso != null) {
+    const dataAtualHoje: Date = new Date();
+    elementoSaldoDataAcesso.textContent = dataAtualHoje.toLocaleDateString("pt-br", {
+        weekday: "long",
+        month: "2-digit",
+        day: "2-digit",
+        year: "numeric"
+    });
 }
